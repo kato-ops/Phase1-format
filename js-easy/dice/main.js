a96728d22da942e9dac5ccbe24d1fb4f801dda11
kato-ops <deepsea_turtle@icloud.com>
2024-10-28 13:38:57 +0900

    dice修正

--- js-easy/dice/main.js
+++ js-easy/dice/main.js
@@ -34,73 +34,58 @@ const setPlayer1dice = document.getElementById("setPlayer1dice");
 const setPlayer2dice = document.getElementById("setPlayer2dice");
 const gameResult = document.getElementById("result");
 
-setPlayer1dice.setAttribute("src", "./img/saikoro1.png")
-setPlayer2dice.setAttribute("src", "./img/saikoro1.png")
+setPlayer1dice.setAttribute("src", "./img/saikoro1.png");
+setPlayer2dice.setAttribute("src", "./img/saikoro1.png");
 
-//addEventListenerに引数を渡す際、変数ではなく値が渡されるので、プレイヤー番号で管理するために配列化
+//プレイヤー番号で管理するために配列化
 //player1 = 0 , player2 = 1
 let playerNum = [0, 0];
 
-const gameRandom = (playerR, diceR) => {
-    playerNum[playerR] = Math.floor(Math.random() * 6 + 1)
-    diceR.setAttribute("src", `./img/saikoro${playerNum[playerR]}.png`);
+const gameRandom = (player, dice) => {
+    playerNum[player] = Math.floor(Math.random() * 6 + 1);
+    dice.setAttribute("src", `./img/saikoro${playerNum[player]}.png`);
 };
 
 //addEventListenerに引数を渡す際、プレイヤー番号を渡していた為、配列で管理
 let playerRolled = [false, false];
 
-//引数をplayer diceといった言葉で統一した方が定義としては分かりやすいと思うのですが、
-//コードとしてみた場合には同じ単語が並んで分かりづらくなると思い、末尾に文字を入れました。
-//どちらの方が良いでしょうか？
-const gameRoll = (btnG, playerG, diceG) => {
-    if (btnG.disabled === false) {
+const gameRoll = (btn, player, dice) => {
+    if (btn.disabled === false) {
         gameResult.textContent = "???";
 
-        const timer = setInterval(gameRandom, 100, playerG, diceG);
-        btnG.disabled = true;
+        const timer = setInterval(gameRandom, 100, player, dice);
+        btn.disabled = true;
 
-        //要らないとは思いますが渡していることが明示的に分かるため一応引数として渡しています。
-        setTimeout((timerS, playerS) => {
-            clearInterval(timerS);
-            playerRolled[playerS] = true;
+        setTimeout(() => {
+            clearInterval(timer);
+            playerRolled[player] = true;
 
             if (playerRolled[0] === true && playerRolled[1] === true) {
                 if (playerNum[0] === playerNum[1]) {
-                    gameResult.textContent = "引き分け"
+                    gameResult.textContent = "引き分け";
                 }
                 else if (playerNum[0] > playerNum[1]) {
-                    gameResult.textContent = "player1の勝利"
+                    gameResult.textContent = "player1の勝利";
                 }
                 else {
-                    gameResult.textContent = "player2の勝利"
+                    gameResult.textContent = "player2の勝利";
                 }
 
                 player1Btn.disabled = false;
                 player2Btn.disabled = false;
                 playerRolled = [false, false];
             }
-        }, 3000, timer, playerG);
+        }, 3000);
     }
 };
 
-//アロー関数の場合、thisで参照できないためラップ
-function gameRollEvent(e) {
-    gameRoll(this.btn, this.player, this.dice);
-}
+const gameRoll1 = () => {
+    gameRoll(player1Btn, 0, setPlayer1dice);
 
-player1Btn.addEventListener("click", { handleEvent: gameRollEvent, btn: player1Btn, player: 0, dice: setPlayer1dice });
-player2Btn.addEventListener("click", { handleEvent: gameRollEvent, btn: player2Btn, player: 1, dice: setPlayer2dice });
-//通常、addEventListenerでは関数に引数を渡す処理は行われない。
-//handleEventがあるオブジェクトをaddEventListenerに渡すことで、関数以外の情報も処理させることが出来ている。
-//処理としては以下のものと同様
-//
-// const foo = {
-//     handleEvent: gameRollEvent,
-//     btn: player1Btn,
-//     player: 0,
-//     dice: setPlayer1dice
-// };
-// player1Btn.addEventListener("click", foo);
-//
-//     num: player1Num
-//　　　といった風にしたかったが、数値はコピーされる際に値がコピーされるため難しい。
+};
+player1Btn.addEventListener("click", gameRoll1);
+
+const gameRoll2 = () => {
+    gameRoll(player2Btn, 1, setPlayer2dice);
+};
+player2Btn.addEventListener("click", gameRoll2);
