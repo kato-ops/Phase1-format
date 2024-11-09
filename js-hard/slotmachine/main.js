1196f69b615d13ecb90a8cd59ace3984900dc7ed
kato-ops <deepsea_turtle@icloud.com>
2024-11-09 21:56:00 +0900

    スロット_書き方修正

--- js-hard/slotmachine/main.js
+++ js-hard/slotmachine/main.js
@@ -1,103 +1,94 @@
-//要素をオブジェクト化するといい
-//array.foreachとか使ったほうが良い
-//forで配列回すと読む時にあの値は何で…とか考える必要があるので
-
-const nowTime = [
-    //slotクラスの子要素pの1番目
-    Array.from(document.querySelectorAll(".slot p:nth-child(1)")),
-    //とにかく子要素のという意味合いでスペースを開けても出来る
-    //スペース開け忘れると、slotの中で何番目のを取ってしまうので注意
-    Array.from(document.querySelectorAll(".slot :nth-child(2)")),
-    //スプレッド構文でバラして配列に格納
-    [...document.querySelectorAll(".slot p:nth-child(3)")]
-];
-
-// const setTime = [
-//     document.getElementById("setTime1"),
-//     document.getElementById("setTime2"),
-//     document.getElementById("setTime3")
-// ];
-const setTime = [...document.querySelectorAll("input:nth-child(-n+3)")];
-
-const startTimer = document.getElementById("startTimer");
-
-let nowNumber = [
-    [9, 0, 1],
-    [9, 0, 1],
-    [9, 0, 1]
-];
-
-let spinId = [];
-let isSpin = [false, false, false];
-
-for (let column = 0; column < 3; column++) {
-    for (let row = 0; row < 3; row++) {
-        nowTime[column][row].textContent = nowNumber[column][row];
-    }
-    setTime[column].disabled = true;
-}
-
-const spinNumber = (column) => {
-    for (let row = 0; row < 3; row++) {
-        if (nowNumber[column][row] <= 0) {
-            nowNumber[column][row] = 9;
-        }
-        else {
-            nowNumber[column][row]--;
-        }
-        nowTime[column][row].textContent = nowNumber[column][row];
-    }
-};
-
-startTimer.addEventListener("click", () => {
-    for (let column = 0; column < 3; column++) {
-        if (isSpin[column] === false) {
-            spinId[column] = setInterval(() => spinNumber(column), 100);
-            isSpin[column] = true;
-        }
-        for (let row = 0; row < 3; row++) {
-            nowTime[column][row].style.backgroundColor = "transparent";
-        }
-        setTime[column].disabled = false;
+//スロットの数値要素など＋縦列のボタンを収めたオブジェクト
+const slotObject = [1, 2, 3].map((col) => {
+    return {
+        row: [1, 2, 3].map((row) => {
+            return {
+                slot: document.querySelectorAll(`.slot p:nth-child(${col})`)[row - 1],
+                number: row
+            }
+        }),
+        stopButton: document.querySelector(`input:nth-child(${col})`),
+        spinId: null,
+        isSpin: false
     }
 });
 
-for (let column = 0; column < 3; column++) {
-    setTime[column].addEventListener("click", () => {
-        clearInterval(spinId[column]);
-        isSpin[column] = false;
-        setTime[column].disabled = true;
+const startTimer = document.getElementById("startTimer");
 
-        //Array.every(判定式) 判定式で 値 === false かを見たい
-        //falseならtrueを返したいので!で反転させれば良い
-        if (isSpin.every(ele => !ele)) {
-            if (nowNumber[0][1] === nowNumber[1][1] && nowNumber[1][1] === nowNumber[2][1]) {
-                nowTime[0][1].style.backgroundColor = "orange";
-                nowTime[1][1].style.backgroundColor = "orange";
-                nowTime[2][1].style.backgroundColor = "orange";
-                setTimeout(() => {
-                    window.alert("おめでとう");
-                }, 0);
-            }
-            else if (nowNumber[0][0] === nowNumber[1][1] && nowNumber[1][1] === nowNumber[2][2]) {
-                nowTime[0][0].style.backgroundColor = "orange";
-                nowTime[1][1].style.backgroundColor = "orange";
-                nowTime[2][2].style.backgroundColor = "orange";
-                setTimeout(() => {
-                    window.alert("おめでとう");
-                }, 0);
-            }
-            else if (nowNumber[0][2] === nowNumber[1][1] && nowNumber[1][1] === nowNumber[2][0]) {
-                nowTime[0][2].style.backgroundColor = "orange";
-                nowTime[1][1].style.backgroundColor = "orange";
-                nowTime[2][0].style.backgroundColor = "orange";
-                setTimeout(() => {
-                    window.alert("おめでとう");
-                }, 0);
+const spinNumber = (ele) => {
+    ele.row.forEach((row) => {
+        if (row.number <= 0) {
+            row.number = 9;
+        }
+        else {
+            row.number--;
+        }
+        row.slot.textContent = row.number;
+    });
+};
+
+startTimer.addEventListener("click", () => {
+    slotObject.forEach((col) => {
+        if (col.isSpin === false) {
+            col.spinId = setInterval(() => spinNumber(col), 100);
+            col.isSpin = true;
+        }
+        col.row.forEach((row) => {
+            row.slot.style.backgroundColor = "transparent";
+        });
+        col.stopButton.disabled = false;
+    });
+});
+
+const hitSlot = (obj) => {
+    const rightDiagonal = obj.map((col, i) => {
+        return col.row[i];
+    });
+    const centerLine = obj.map((col) => {
+        return col.row[1];
+    });
+    const leftDiagonal = obj.map((col, i) => {
+        const len = 2;
+        return col.row[len - i];
+    });
+
+    if (rightDiagonal.every((ele) => ele.number === rightDiagonal[0].number)) {
+        return rightDiagonal;
+    }
+    else if (centerLine.every((ele) => ele.number === centerLine[0].number)) {
+        return centerLine;
+    }
+    else if (leftDiagonal.every((ele) => ele.number === leftDiagonal[0].number)) {
+        return leftDiagonal;
+    }
+    else {
+        return false;
+    }
+};
+
+slotObject.forEach((col) => {
+    //初期化
+    col.row.forEach((row) => {
+        row.slot.textContent = row.number;
+    });
+    col.stopButton.disabled = true;
+
+    //ストップボタンの処理
+    col.stopButton.addEventListener("click", () => {
+        clearInterval(col.spinId);
+        col.isSpin = false;
+        col.stopButton.disabled = true;
+        if (slotObject.every((ele) => !ele.isSpin)) {
+            const result = hitSlot(slotObject);
+            if (result !== false) {
+                result.forEach((ele) => {
+                    ele.slot.style.backgroundColor = "orange"
+                });
+                setTimeout(() => window.alert("おめでとう"), 2);
             }
             else {
                 window.alert("再挑戦");
             }
         }
     });
-}
+});
