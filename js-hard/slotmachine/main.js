db91a8c6cd6a449f2664512007632854d33e81a9
kato-ops <deepsea_turtle@icloud.com>
2024-11-20 20:51:54 +0900

    スロット_修正

--- js-hard/slotmachine/main.js
+++ js-hard/slotmachine/main.js
@@ -1,70 +1,69 @@
-const columnElements = new Array(3).fill(0).map((_, i) => i + 1);
-const rowElements = new Array(3).fill(0).map((_, i) => i + 1);
+const reelSize = 3;
+const SymbolSize = 3;
 
-//スロットの数値要素など＋縦列のボタンを収めたオブジェクト
-//オブジェクトとか中身的な意味合いじゃなくて、概念的に何か？を変数名にした方が良い
-const slotObject = columnElements.map((col) => {
+const slot = new Array(reelSize).fill(0).map((_, reelIndex) => {
+    const buttonElement = document.querySelector(`input:nth-child(${reelIndex + 1})`);
+    buttonElement.disabled = true;
     return {
-        //rowか？
-        row: rowElements.map((row) => {
+        symbols: new Array(SymbolSize).fill(0).map((_, symbolIndex) => {
+            const symbolElement = document.querySelectorAll(`.slot p:nth-child(${reelIndex + 1})`)[symbolIndex];
+            symbolElement.textContent = symbolIndex;
             return {
-                //これは要素だからそうした方が…
-                slot: document.querySelectorAll(`.slot p:nth-child(${col})`)[row - 1],
-                number: row
-            }
+                element: symbolElement,
+                number: symbolIndex
+            };
         }),
-        stopButton: document.querySelector(`input:nth-child(${col})`),
+        stopButton: buttonElement,
         spinId: null,
         isSpin: false
-    }
+    };
 });
 
-const startTimer = document.getElementById("startTimer");
+const startButton = document.getElementById("startTimer");
 
-//仮引数も、それ単体で何入れれば良いのか分かるように
-const spinNumber = (ele) => {
-    ele.row.forEach((row) => {
-        if (row.number <= 0) {
-            row.number = 9;
+const spinReel = (reel) => {
+    reel.symbols.forEach((content) => {
+        if (content.number <= 0) {
+            content.number = 9;
         }
         else {
-            row.number--;
+            content.number--;
         }
-        row.slot.textContent = row.number;
+        content.element.textContent = content.number;
     });
 };
 
-startTimer.addEventListener("click", () => {
-    slotObject.forEach((col) => {
-        if (col.isSpin === false) {
-            col.spinId = setInterval(() => spinNumber(col), 100);
-            col.isSpin = true;
+startButton.addEventListener("click", () => {
+    slot.forEach((reel) => {
+        if (reel.isSpin === false) {
+            reel.spinId = setInterval(() => spinReel(reel), 1000);
+            reel.isSpin = true;
         }
-        col.row.forEach((row) => {
-            row.slot.style.backgroundColor = "transparent";
+        reel.symbols.forEach((content) => {
+            content.element.style.backgroundColor = "transparent";
         });
-        col.stopButton.disabled = false;
+        reel.stopButton.disabled = false;
     });
 });
 
-const hitSlot = (obj) => {
-    const rightDiagonal = obj.map((col, i) => {
-        return col.row[i];
+const hitSlot = () => {
+    const rightDiagonal = slot.map((reel, index) => {
+        return reel.symbols[index];
     });
-    const centerLine = obj.map((col) => {
-        return col.row[1];
+    const centerLine = slot.map((reel) => {
+        return reel.symbols[1];
     });
-    const leftDiagonal = obj.map((col, i) => {
-        return col.row[obj.length - 1 - i];
+    const leftDiagonal = slot.map((reel, index) => {
+        return reel.symbols[slot.length - 1 - index];
     });
 
-    if (rightDiagonal.every((ele) => ele.number === rightDiagonal[0].number)) {
+    if (rightDiagonal.every((content) => content.number === rightDiagonal[0].number)) {
         return rightDiagonal;
     }
-    else if (centerLine.every((ele) => ele.number === centerLine[0].number)) {
+    else if (centerLine.every((content) => content.number === centerLine[0].number)) {
         return centerLine;
     }
-    else if (leftDiagonal.every((ele) => ele.number === leftDiagonal[0].number)) {
+    else if (leftDiagonal.every((content) => content.number === leftDiagonal[0].number)) {
         return leftDiagonal;
     }
     else {
@@ -72,23 +71,16 @@ const hitSlot = (obj) => {
     }
 };
 
-slotObject.forEach((col) => {
-    //初期化
-    col.row.forEach((row) => {
-        row.slot.textContent = row.number;
-    });
-    col.stopButton.disabled = true;
-
-    //ストップボタンの処理
-    col.stopButton.addEventListener("click", () => {
-        clearInterval(col.spinId);
-        col.isSpin = false;
-        col.stopButton.disabled = true;
-        if (slotObject.every((ele) => !ele.isSpin)) {
-            const result = hitSlot(slotObject);
+slot.forEach((reel) => {
+    reel.stopButton.addEventListener("click", () => {
+        clearInterval(reel.spinId);
+        reel.isSpin = false;
+        reel.stopButton.disabled = true;
+        if (slot.every((reel) => !reel.isSpin)) {
+            const result = hitSlot();
             if (result !== false) {
-                result.forEach((ele) => {
-                    ele.slot.style.backgroundColor = "orange"
+                result.forEach((reel) => {
+                    reel.element.style.backgroundColor = "orange"
                 });
                 setTimeout(() => window.alert("おめでとう"), 2);
             }
