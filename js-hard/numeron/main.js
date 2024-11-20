e367970af1662b01e0f7a5cfa7e6b49b8a9ee7dc
kato-ops <deepsea_turtle@icloud.com>
2024-11-20 19:59:17 +0900

    ヌメロン_修正

--- js-hard/numeron/main.js
+++ js-hard/numeron/main.js
@@ -1,29 +1,20 @@
-//配列に空の使わないのは要らない
-let cpNum = new Array(3);
-let userNum = new Array(3);
-let remTurns = 10;
+let cpNum;
+let remTurns;
 
-const answerNum = document.getElementById("answerNum");
-const numCheck = document.getElementById("numCheck");
-const remainTurn = document.getElementById("remainTurn");
+const inputNum = document.getElementById("answerNum");
+const answerButton = document.getElementById("numCheck");
+const displayTurn = document.getElementById("remainTurn");
 
 const hasDuplicate = (array) => {
     return new Set(array).size !== array.length;
 };
 
 const makeRandNum = () => {
-    let num = [];
-    let limit = 1000;
+    const num = [];
     while (num.length < 3) {
         const rand = Math.floor(Math.random() * 10);
         if (!num.includes(rand)) {
             num.push(rand);
-        }
-
-        limit--;
-        if (limit <= 0) {
-            console.log("ERR:infinite loop");
-            break;
         }
     }
     return num;
@@ -49,15 +40,17 @@ const countEatBite = (arr1, arr2) => {
     return { eat: eatNum, bite: biteNum };
 };
 
-//init処理は処理でまとめたい
-cpNum = makeRandNum();
-console.log(cpNum);
-remainTurn.textContent = `あと残り${remTurns}回です`;
+const initCpNum = () => {
+    cpNum = makeRandNum();
+    console.log(cpNum);
+    remTurns = 10;
+    displayTurn.textContent = `あと残り${remTurns}回です`;
+};
 
-numCheck.addEventListener("click", () => {
-    //この処理中でしか使わないから、constで定義しちゃったほうが
-    //あとtostring要らないらしい。valueは基本文字列だとか
-    userNum = answerNum.value.toString().split(``).map(Number);
+initCpNum();
+
+answerButton.addEventListener("click", () => {
+    const userNum = inputNum.value.split(``).map(Number);
     if (userNum.length !== 3) {
         window.alert("3桁の数を入れて下さい");
         return;
@@ -70,27 +63,20 @@ numCheck.addEventListener("click", () => {
     const result = countEatBite(cpNum, userNum);
     window.alert(`${result.eat} EAT, ${result.bite} BITE`);
 
-    //処理として一まとまりなのはreturnで抜けたりすると逆に分かりにくい
-    if (result.eat === 3) {
-        window.alert("正解です！");
-        cpNum = makeRandNum();
-        console.log(cpNum);
-        remTurns = 10;
-        remainTurn.textContent = `あと残り${remTurns}回です`;
-        return;
-    }
-
-    if (remTurns > 1) {
-        remTurns--;
-        remainTurn.textContent = `あと残り${remTurns}回です`;
-        return;
+    if (result.eat !== 3) {
+        if (remTurns > 1) {
+            remTurns--;
+            displayTurn.textContent = `あと残り${remTurns}回です`;
+            return;
+        }
+        else {
+            window.alert(`終了です。答えは${cpNum.join('')}でした`);
+            initCpNum();
+            return;
+        }
     }
     else {
-        window.alert(`終了です。答えは${cpNum.join('')}でした`);
-        cpNum = makeRandNum();
-        console.log(cpNum);
-        remTurns = 10;
-        remainTurn.textContent = `あと残り${remTurns}回です`;
-        return;
+        window.alert("正解です！");
+        initCpNum();
     }
 });
